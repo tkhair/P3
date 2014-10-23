eabab8afe78d6816bb99e41bf34a762ce2d80471
Tatiana Khair <t.mirkevich@gmail.com>
2014-10-22 20:24:57 -0400

    Live URL added

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -7,12 +7,12 @@ Assignment 3 - Homepage
       <div>
 <h2>Lorem Ipsum Generator</h2>
 <p>In publishing and graphic design, lorem ipsum is a filler text commonly used to demonstrate the graphic elements of a document or visual presentation. Replacing meaningful content that could be distracting with placeholder text may allow viewers to focus on graphic aspects such as font, typography, and page layout.<br/>
-<a href="http://en.wikipedia.org/wiki/Lorem_ipsum">Wikipedia</a><br/>
+&#169; <a href="http://en.wikipedia.org/wiki/Lorem_ipsum">Wikipedia</a><br/>
 This tool will help you to generate desired amount of dummy text content.</p>
-<a href="http://khair.me/lorem">Dummy Text Generating Tool</a><br/>
+<a href="/lorem">Dummy Text Generating Tool</a><br/>
 <br/>
 <h2>Random User Generator</h2>
 <p>Sometimes you may need to fill user profiles with random details. This tool will help you to generat a content of this type.</p>
-<a href="http://khair.me/users">Fake Users Data Generating Tool</a>
+<a href="/users">Fake Users Data Generating Tool</a>
     </div> 
 @stop
